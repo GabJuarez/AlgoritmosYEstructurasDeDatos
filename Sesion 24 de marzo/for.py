6340c5e9c1a2d96a50f404975d240b8d74118ba7
GabJuarez <gajuarez@uamv.edu.ni>
2025-03-24 14:04:17 -0600

    sesion clase del lunes

---
+++ Sesion 24 de marzo/for.py
@@ -0,0 +1,6 @@
+for i in range(10):
+    print(i)
+
+for i in range(10, 51, 2):
+    #primer argumento es el valor inicial de la variable, segundo limite superior, tercero actualizacion del blucle
+    print (i)
